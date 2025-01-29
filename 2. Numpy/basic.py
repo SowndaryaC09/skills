c32d3da50774839794866a496fe5819338cc29f3
SowndaryaC09 <sowndaryac25@gmail.com>
2025-01-29 17:28:20 +0530

    new

---
+++ 2. Numpy/basic.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#----------------------------------------Datatypes and attributes----------------------------------------
+#ndarray 
+a1=np.array([1,2,3])
+a1
+a2=np.array([[1,2,3,6], [4,6,5,9]])    # ([[[]]]) --> 3 dimensional
+a2.ndim
+a2.shape
+a2.size
